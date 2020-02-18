8c94895b08a589d7ba00bf69d644db6a2a414b23
Lou <a.galgao@gs1ph.org>
2020-02-18 14:28:46 +0800

    Change the heading

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="well">
-    <h3>{{$post->title}}</h3>
+    <h1 class="display-3 text-center">{{$post->title}}</h1>
     <p>{!!$post->body!!}</p>
     <p style="text-align:right;font-size:12px;">Written on {{$post->created_at}}</p>
     <br>
